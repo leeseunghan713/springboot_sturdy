12c99ea362ff95d98601ce0a366b3e3500f1d59f
DESKTOP-97HR2Q3\seung <seunghan713@sunmoon.ac.kr>
2024-10-26 14:38:29 +0900

    Chapter2 1일차

--- sbb/src/main/java/com/mysite/sbb/Answer.java
+++ sbb/src/main/java/com/mysite/sbb/Answer.java
@@ -1,2 +1,24 @@
-package com.mysite.sbb;public class Answer {
+package com.mysite.sbb;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
 }
